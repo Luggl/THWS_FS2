701829e8f1b0481f802d05fca63755d4cf0e8aa9
Luggl <l.langpeter@yahoo.de>
2024-04-13 10:38:57 +0200

    .

--- src/main/java/Lec16_1/LaeuferImpl.java
+++ src/main/java/Lec16_1/LaeuferImpl.java
@@ -12,19 +12,15 @@ public class LaeuferImpl extends AbstractFigur implements Laeufer
     Brett brett = new Brett();
     int x_var = x;
     int y_var = y;
-    int countLoop;
+    int count = 0;
     while(x_var < 8 && y_var < 8){
       x_var++;
       y_var++;
     }
 
-    if(x_var<=y_var){
-      countLoop = x_var;
-    }else{
-      countLoop = y_var;
-    }
-    for(int i = 0; i<=countLoop; i++){
-      brett.markiereFeld(x_var-countLoop, y_var-countLoop);
+    while(x_var - count >= 1 && y_var - count >= 1){
+      brett.markiereFeld(x_var - count, y_var - count);
+      count++;
     }
 
     x_var = x;
@@ -34,13 +30,12 @@ public class LaeuferImpl extends AbstractFigur implements Laeufer
       y_var++;
     }
 
-    if(x_var<=y_var){
-      countLoop = x_var;
-    }else{
-      countLoop = y_var;
-    }
-    for(int i = 0; i<=countLoop; i++){
-      brett.markiereFeld(x_var+countLoop, y_var+countLoop);
+    count = 0;
+
+    while(x_var + count <= 8 && y_var - count >1){
+
+      brett.markiereFeld(x_var + count, y_var - count);
+      count++;
     }
 
     return brett;
@@ -48,7 +43,7 @@ public class LaeuferImpl extends AbstractFigur implements Laeufer
   
   public static void main(String[] args)
   {
-    LaeuferImpl l = new LaeuferImpl(4,5);
+    LaeuferImpl l = new LaeuferImpl(3,7);
     Brett brett = l.gibErlaubteFelder();
     for (int j = 1; j <= 8; j++)
     {
