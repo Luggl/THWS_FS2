760ab47eafbd54a1ba5064520b089bded112bf4a
Luggl <l.langpeter@yahoo.de>
2024-06-16 16:38:30 +0200

    k

---
+++ src/main/java/Klausurvorbereitung/Musik/VergleicheMusikStueckLaenge.java
@@ -0,0 +1,16 @@
+package Klausurvorbereitung.Musik;
+
+import java.util.Comparator;
+
+public class VergleicheMusikStueckLaenge implements Comparator<MusikStueck> {
+
+    public int compare(MusikStueck a, MusikStueck b){
+        if(a.laenge<b.laenge){
+            return -1;
+        }else if(a.laenge > b.laenge){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
---
+++ src/main/java/Klausurvorbereitung/Musik/VergleicheMusikStueckTitel.java
@@ -0,0 +1,10 @@
+package Klausurvorbereitung.Musik;
+
+import java.util.Comparator;
+
+public class VergleicheMusikStueckTitel implements Comparator<MusikStueck> {
+
+    public int compare(MusikStueck a, MusikStueck b){
+        return(a.titel.compareTo(b.titel));
+    }
+}
