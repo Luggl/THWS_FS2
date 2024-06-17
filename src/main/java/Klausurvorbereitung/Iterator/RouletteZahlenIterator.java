8e97052e77367083f2163ce46d31bd8b33fb9e45
Luggl <l.langpeter@yahoo.de>
2024-06-17 17:41:04 +0200

    k

---
+++ src/main/java/Klausurvorbereitung/Iterator/RouletteZahlenIterator.java
@@ -0,0 +1,17 @@
+package Klausurvorbereitung.Iterator;
+
+import java.util.Iterator;
+
+public class RouletteZahlenIterator<E> implements Iterator<E> {
+
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public E next() {
+        return null;
+    }
+}
